3717754d2d7312372405fb142ede5a9cb9806514
Илья Ястребилов <yastrebilov2005@mail.ru>
2024-02-13 12:15:36 +0700

    listbox

--- Programming/View/MainForm.Designer.cs
+++ Programming/View/MainForm.Designer.cs
@@ -28,19 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            ValuesListBox = new ListBox();
+            EnumsListBox = new ListBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
             SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 450);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(ValuesListBox);
+            tabPage1.Controls.Add(EnumsListBox);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 422);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Enums";
+            tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(390, 3);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Int value:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(198, 3);
+            label2.Name = "label2";
+            label2.Size = new Size(81, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Choose value:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(8, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Choose enumeration:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(390, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(145, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // ValuesListBox
+            // 
+            ValuesListBox.FormattingEnabled = true;
+            ValuesListBox.ItemHeight = 15;
+            ValuesListBox.Location = new Point(198, 22);
+            ValuesListBox.Name = "ValuesListBox";
+            ValuesListBox.Size = new Size(175, 229);
+            ValuesListBox.TabIndex = 1;
+            ValuesListBox.SelectedIndexChanged += ValuesListBox_SelectedIndexChanged;
+            // 
+            // EnumsListBox
+            // 
+            EnumsListBox.FormattingEnabled = true;
+            EnumsListBox.ItemHeight = 15;
+            EnumsListBox.Items.AddRange(new object[] { "Colours", "FormOfTheStudentEducation", "Genre", "SeasonOfYear", "SmartphoneManufacturers", "Weekday" });
+            EnumsListBox.Location = new Point(8, 22);
+            EnumsListBox.Name = "EnumsListBox";
+            EnumsListBox.Size = new Size(175, 229);
+            EnumsListBox.TabIndex = 0;
+            EnumsListBox.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
             Name = "MainForm";
-            Text = "Form1";
+            Text = "Programming Demo";
             Load += MainForm_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private ListBox ValuesListBox;
+        private ListBox EnumsListBox;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
     }
 }
--- Programming/View/MainForm.cs
+++ Programming/View/MainForm.cs
@@ -5,9 +5,26 @@ namespace Programming
         public MainForm()
         {
             InitializeComponent();
+            OutputParseLabel.Text = "";
         }
 
         private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EnumsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = EnumsListBox.SelectedIndex;
+            object[] values = Enum.GetValues(_typeModel[selectedIndex]).Cast<object>().ToArray();
+        }
+
+        private void Enums_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ValuesListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
