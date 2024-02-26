c9e57d25958286245c4faf103c7107c2e0af2a3a
Илья Ястребилов <yastrebilov2005@mail.ru>
2024-02-26 21:21:40 +0700

    tried to parse

--- Programming/View/MainForm.cs
+++ Programming/View/MainForm.cs
@@ -1,3 +1,5 @@
+using Programming.Model;
+
 namespace Programming
 {
     public partial class MainForm : Form
@@ -5,8 +7,10 @@ namespace Programming
         public MainForm()
         {
             InitializeComponent();
-            OutputParseLabel.Text = "";
         }
+        private readonly Type[] _typeModel = new Type[] { typeof(Colours),
+        typeof(FormOfTheStudentEducation), typeof(Genre), typeof (SeasonOfYear),
+        typeof(SmartphoneManufacturers), typeof(Weekday)};
 
         private void MainForm_Load(object sender, EventArgs e)
         {
@@ -15,8 +19,6 @@ namespace Programming
 
         private void EnumsListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int selectedIndex = EnumsListBox.SelectedIndex;
-            object[] values = Enum.GetValues(_typeModel[selectedIndex]).Cast<object>().ToArray();
         }
 
         private void Enums_Click(object sender, EventArgs e)
@@ -25,6 +27,46 @@ namespace Programming
         }
 
         private void ValuesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = ValuesListBox.SelectedIndex;
+            textBox1.Text = selectedIndex.ToString();
+        }
+
+        private void EnumsListBox_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            int selectedIndex = EnumsListBox.SelectedIndex;
+            object[] values = Enum.GetValues(_typeModel[selectedIndex]).Cast<object>().ToArray();
+            ValuesListBox.Items.Clear();
+            ValuesListBox.Items.AddRange(values);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Parse_Click(object sender, EventArgs e)
+        {
+            Weekday week;
+            string day = textBox2.Text;
+            if (Enum.TryParse<Weekday>(day, out week))
+                object value = enum.Parse(typeof(Weekday));
+
+            textBox3.Text = ("Это день недели ({0} = {1})", day, Enum.Parse(typeof(Weekday), day));
+
+        }
+
+        private void Weekday_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResultOfWeekday_TextChanged(object sender, EventArgs e)
         {
 
         }
