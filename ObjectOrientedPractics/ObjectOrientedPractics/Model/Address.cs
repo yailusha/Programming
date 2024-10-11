b65ad145a9d1db3024267f041c3f2460848e8550
Илья Ястребилов <yastrebilov2005@mail.ru>
2024-10-11 09:21:12 +0700

    lol what to do with address

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Address.cs
@@ -0,0 +1,96 @@
+﻿using ObjectOrientedPractics.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Address
+    {
+        private int _index;
+        private string _country;
+        private string _city;
+        private string _street;
+        private string _building;
+        private string _apartment;
+        public int Index
+        {
+            get { return _index; }
+            set
+            {
+                if (ValueValidator.AssertValueInRange(value, 100000, 999999, nameof(Index)))
+                {
+                    _index = value;
+                }
+            }
+        }
+        public string Country
+        {
+            get { return _country; }
+            set
+            {
+                if (ValueValidator.AssertStringOnLength(value, 50, nameof(Country)))
+                {
+                    _country = value;
+                }
+            }
+        }
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                if (ValueValidator.AssertStringOnLength(value, 50, nameof(City)))
+                {
+                    _city = value;
+                }
+            }
+        }
+        public string Street
+        {
+            get { return _street; }
+            set
+            {
+                if (ValueValidator.AssertStringOnLength(value, 100, nameof(Street)))
+                {
+                    _street = value;
+                }
+            }
+        }
+        public string Building
+        {
+            get { return _building; }
+            set
+            {
+                if (ValueValidator.AssertStringOnLength(value, 10, nameof(Building)))
+                {
+                    _building = value;
+                }
+            }
+        }
+        public string Apartment
+        {
+            get { return _apartment; }
+            set
+            {
+                if (ValueValidator.AssertStringOnLength(value, 10, nameof(Apartment)))
+                {
+                    _apartment = value;
+                }
+            }
+        }
+        public Address (int index, string country, string city, string street, string building, string apartment)
+        {
+            Index = index;
+            Country = country;
+            City = city;
+            Street = street;
+            Building = building;
+            Apartment = apartment;
+        }
+        public Address()
+        { }
+    }
+}
---
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/Controls/AddressControl.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace ObjectOrientedPractics.View.Controls
+{
+    partial class AddressControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            postIndexTextBox = new TextBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(15, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Delivery Address";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(15, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Post Index:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(15, 71);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Country:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(15, 102);
+            label4.Name = "label4";
+            label4.Size = new Size(40, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Street:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(15, 133);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Building:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(187, 133);
+            label6.Name = "label6";
+            label6.Size = new Size(67, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Apartment:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(291, 71);
+            label7.Name = "label7";
+            label7.Size = new Size(31, 15);
+            label7.TabIndex = 6;
+            label7.Text = "City:";
+            // 
+            // postIndexTextBox
+            // 
+            postIndexTextBox.Location = new Point(86, 32);
+            postIndexTextBox.Name = "postIndexTextBox";
+            postIndexTextBox.Size = new Size(131, 23);
+            postIndexTextBox.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(86, 63);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(181, 23);
+            textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(328, 68);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(181, 23);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(86, 99);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(423, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(86, 130);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(95, 23);
+            textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(260, 130);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(95, 23);
+            textBox5.TabIndex = 12;
+            // 
+            // AddressControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(postIndexTextBox);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "AddressControl";
+            Size = new Size(554, 205);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox postIndexTextBox;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+    }
+}
