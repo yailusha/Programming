fb0a4fcc3116d8dda64cc4d79e08a3ef6218bdab
Илья Ястребилов <yastrebilov2005@mail.ru>
2024-10-11 09:21:12 +0700

    lol what to do with address

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/Controls/AddressControl.cs
@@ -0,0 +1,24 @@
+﻿using ObjectOrientedPractics.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectOrientedPractics.View.Controls
+{
+    public partial class AddressControl : UserControl
+    {
+        private Address _address;
+        public AddressControl()
+        {
+            InitializeComponent();
+            _address = new Address();
+        }
+        public Address Address { get; set; }
+    }
+}
