605d8b1ddad6370a62fb965d1d337a260d827ab8
Илья Ястребилов <yastrebilov2005@mail.ru>
2024-10-17 21:11:16 +0700

    lab2 is done

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/Controls/AddressControl.cs
@@ -0,0 +1,146 @@
+﻿using ObjectOrientedPractics.Model;
+using ObjectOrientedPractics.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectOrientedPractics.View.Controls
+{
+    /// <summary>
+    /// Хранит данные об адресе покупателя
+    /// </summary>
+    public partial class AddressControl : UserControl
+    {
+        private Address _address = new Address();
+        public Address Address
+        {
+            get { return _address; }
+            set
+            {
+                _address = value;
+                postIndexTextBox.Text = _address.Index.ToString();
+                countryTextBox.Text = _address.Country;
+                cityTextBox.Text = _address.City;
+                streetTextBox.Text = _address.Street;
+                buildingTextBox.Text = _address.Building;
+                apartmentTextBox.Text = _address.Apartment;
+            }
+        }
+        public AddressControl()
+        {
+            InitializeComponent();
+            this.toolTip1 = new ToolTip();
+        }
+        public void ClearInfo()
+        {
+            postIndexTextBox.Clear();
+            countryTextBox.Clear();
+            cityTextBox.Clear();
+            streetTextBox.Clear();
+            buildingTextBox.Clear();
+            apartmentTextBox.Clear();
+            _address = new Address();
+        }
+        private void postIndexTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                postIndexTextBox.BackColor = Color.White;
+                int postIndex = int.Parse(postIndexTextBox.Text);
+                _address.Index = postIndex;
+            }
+            catch 
+            {
+                postIndexTextBox.BackColor = Color.LightPink;
+                this.toolTip1.SetToolTip(this.postIndexTextBox, "Почтовый индекс должен быть шестизначным числом");
+                
+            }
+        }
+
+        private void countryTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                countryTextBox.BackColor = Color.White;
+                string country = countryTextBox.Text;
+                _address.Country = country;
+            }
+            catch
+            {
+                countryTextBox.BackColor = Color.LightPink;
+                this.toolTip1.SetToolTip(this.countryTextBox, "Длина страны может содержать максимум 50 символов.");
+            }
+        }
+
+        private void cityTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                cityTextBox.BackColor = Color.White;
+                string city = cityTextBox.Text;
+                cityTextBox.Text = city.ToString();              
+                _address.City = city;
+            }
+            catch 
+            {
+                cityTextBox.BackColor = Color.LightPink;
+                this.toolTip1.SetToolTip(this.countryTextBox, "Длина строки может содержать максимум 50 символов");
+            }
+        }
+
+        private void streetTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                streetTextBox.BackColor = Color.White;
+                string street = streetTextBox.Text;
+                streetTextBox.Text = street.ToString();                          
+                _address.Street = street;
+            }
+            catch
+            {
+                streetTextBox.BackColor = Color.LightPink;
+                this.toolTip1.SetToolTip(this.streetTextBox, "Длина строки может содержать максимум 100 символов");
+            }
+        }
+
+        private void buildingTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                buildingTextBox.BackColor = Color.White;
+                string building = buildingTextBox.Text;  
+                buildingTextBox.Text = building.ToString();
+                _address.Building = building;
+            }
+            catch 
+            {
+                buildingTextBox.BackColor = Color.LightPink;
+                this.toolTip1.SetToolTip(this.buildingTextBox, "Длина строки может содержать максимум 10 символов");
+            }
+        }
+
+        private void apartmentTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                apartmentTextBox.BackColor = Color.White;
+                string apartment = apartmentTextBox.Text;
+                apartmentTextBox.Text = apartment.ToString();             
+                _address.Apartment = apartment;
+            }
+            catch 
+            {
+                apartmentTextBox.BackColor = Color.LightPink;
+                this.toolTip1.SetToolTip(apartmentTextBox, "Длина строки может содержать максимум 10 символов");
+            }
+        }
+
+    }
+}
--- ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.cs
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.cs
@@ -1,10 +1,17 @@
+using ObjectOrientedPractics.Model;
+
 namespace ObjectOrientedPractics
 {
     public partial class MainForm : Form
     {
+        private Store _store = new Store();
         public MainForm()
         {
             InitializeComponent();
+            _store.Items = new List<Item>();
+            _store.Customers = new List<Customer>();
+            itemsTab1.Items = _store.Items;
+            customersTab1.Customers = _store.Customers;
         }
     }
 }
