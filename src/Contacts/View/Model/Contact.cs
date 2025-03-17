6f4f09b2f843a679184a51f92f7241d9cbded081
Илья Ястребилов <yastrebilov2005@mail.ru>
2025-03-17 18:21:54 +0700

    feat(class): add Contact and ValueValidator

---
+++ src/Contacts/View/Model/Contact.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using View.Model.Services;
+
+namespace View.Model
+{
+    internal class Contact
+    {
+        private string _name;
+        private string _phoneNumber;
+        private string _email;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == value) return;
+                ValueValidator.AssertStringOnLength(value, 100, nameof(Name));
+                _name = value;
+            }
+        }
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                if (_phoneNumber == value) return;
+                ValueValidator.AssertStringOnMinLength(value, 11, nameof(PhoneNumber));
+                _phoneNumber = value;
+            }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (_email == value) return;
+                if (!value.Contains("@"))
+                {
+                    throw new ArgumentException("Email doesn't contain \"@\"");
+                }
+                ValueValidator.AssertStringOnLength(value, 100, nameof(Email));
+                _email = value;
+            }
+        }
+        public Contact (string name, string phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+        public Contact()
+        {
+
+        }
+    }
+}
---
+++ src/Contacts/View/Model/Services/ValueValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model.Services
+{
+    static class ValueValidator
+    {
+        public static bool AssertStringOnLength(string value, int maxLength, string propertyName)
+        {
+            if (value.Length > maxLength || value.Length == 0)
+            {
+                throw new ArgumentException($"Too long {propertyName}. It can be maximum {maxLength} symbols.");
+            }
+            return true;
+        }
+        public static bool AssertStringOnMinLength(string value, int minLength, string propertyName)
+        {
+            if (value.Length != minLength)
+            {
+                throw new ArgumentException($"Not enough length {propertyName}. It must has {minLength} symbols.");
+            }
+            return true;
+        }
+    }
+}
