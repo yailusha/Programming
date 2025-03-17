441495235fcaa83f358964fdc39367a157142351
Илья Ястребилов <yastrebilov2005@mail.ru>
2025-03-17 23:16:31 +0700

    feat(class): add ContactSerializer

---
+++ src/Contacts/View/Model/Services/ContactSerializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace View.Model.Services
+{
+    class ContactSerializer
+    {
+        private string filePath = Path.Combine((Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)),
+            "Contacts", "contacts.json");
+
+        public void SaveFile(Contact contact)
+        {
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(contact);
+            System.IO.File.WriteAllText(filePath, json);
+        }
+
+        public Contact LoadFile()
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("File was not found.");
+            }
+            string json = File.ReadAllText(filePath);
+            Contact contact = Newtonsoft.Json.JsonConvert.DeserializeObject<Contact>(json);
+            return contact;
+        }
+    }
+}
