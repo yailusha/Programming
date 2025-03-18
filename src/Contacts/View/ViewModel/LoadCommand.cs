5144b3872f48933445d04760e96046e9e6792f63
Илья Ястребилов <yastrebilov2005@mail.ru>
2025-03-18 22:48:37 +0700

    feat(class): add new classed LoadCommand and SaveCommand

---
+++ src/Contacts/View/ViewModel/LoadCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using View.Model.Services;
+using View.Model;
+using System.Net.Http.Headers;
+
+namespace View.ViewModel
+{
+    internal class LoadCommand : ICommand
+    {
+        private ContactSerializer _serializer;
+        private MainVM _mainVM;
+
+        public LoadCommand(ContactSerializer serializer, MainVM mainVM)
+        {
+            _serializer = serializer;
+            _mainVM = mainVM;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            try
+            {
+                Contact contact = _serializer.LoadFile();
+                _mainVM.Name = contact.Name;
+                _mainVM.PhoneNumber = contact.PhoneNumber;
+                _mainVM.Email = contact.Email;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Data could not be load from file.");
+            }
+        }
+    }
+}
---
+++ src/Contacts/View/ViewModel/SaveCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using View.Model;
+using View.Model.Services;
+
+namespace View.ViewModel
+{
+    internal class SaveCommand : ICommand
+    {
+        private ContactSerializer _serializer;
+        private Contact _contact;
+
+        public SaveCommand(ContactSerializer serializer, Contact contact)
+        {
+            _serializer = serializer;
+            _contact = contact;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            _serializer.SaveFile(_contact);
+        }
+    }
+}
+
