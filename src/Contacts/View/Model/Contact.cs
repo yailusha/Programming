b27a899871abee92b9e3e5518a159ba9466928d9
Илья Ястребилов <yastrebilov2005@mail.ru>
2025-05-17 17:05:15 +0700

    feat: add validataion in contact and image

--- src/Contacts/View/Model/Contact.cs
+++ src/Contacts/View/Model/Contact.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
 
 namespace Model
 {
     /// <summary>
     /// Хранит информацию о контактных данных человека.
     /// </summary>
-    internal class Contact
+    internal class Contact : IDataErrorInfo
     {
         /// <summary>
         /// Имя человека.
@@ -70,7 +71,7 @@ namespace Model
         /// <param name="name">Имя.</param>
         /// <param name="phoneNumber">Номер телефона.</param>
         /// <param name="email">Электронная почта.</param>
-        public Contact (string name, string phoneNumber, string email)
+        public Contact(string name, string phoneNumber, string email)
         {
             Name = name;
             PhoneNumber = phoneNumber;
@@ -81,6 +82,61 @@ namespace Model
         {
 
         }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = string.Empty;
+                switch (columnName)
+                {
+                    case nameof(Name):
+                        {
+                            if (!string.IsNullOrEmpty(Name))
+                            {
+                                if (Name.Length > 100)
+                                {
+                                    error = "Length of name must be 100 or less";
+                                }
+                            }
+                            break;
+                        }
+                    case nameof(PhoneNumber):
+                        {
+                            if (!string.IsNullOrEmpty(PhoneNumber))
+                            {
+                                if (PhoneNumber.Length > 100)
+                                {
+                                    error = "Length of phone number must be 100 or less";
+                                }
+                                if (!Regex.IsMatch(PhoneNumber, @"^[0-9+\-()\s]+$"))
+                                {
+                                    error = "Phone number can contain only +()- or digits";
+                                }
+                            }
+                            break;
+                        }
+                    case nameof(Email):
+                        {
+                            if (!string.IsNullOrEmpty(Email))
+                            {
+                                if (Email.Length > 100)
+                                {
+                                    error = "Length of email must be 100 or less";
+                                }
+                                if (!Email.Contains("@"))
+                                {
+                                    error = "Email must contain @";
+                                }
+                            }
+                            break;
+                        }
+                }
+                return error;
+            }
+        }
+
+        public string Error => null;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
