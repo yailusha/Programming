935adbc79ce6907992fde18a1325fbd45d854eb5
Илья Ястребилов <yastrebilov2005@mail.ru>
2025-05-17 17:29:51 +0700

    feat: add pasting in phonenumber at the ContactControl

---
+++ src/Contacts/View/Controls/ContactControl.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace View.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+
+        private static Regex _regex = new Regex(@"^[0-9+\-()\s]+$");
+
+        public ContactControl()
+        {
+            InitializeComponent();
+            PhoneNumberTextBox.PreviewTextInput += PhoneNumberTextBox_PreviewTextInput;
+            DataObject.AddPastingHandler(PhoneNumberTextBox, PhoneNumberTextBox_Pasting);
+        }
+
+        private void PhoneNumberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !_regex.IsMatch(e.Text);
+        }
+
+        private void PhoneNumberTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string phoneNumber = (string)e.DataObject.GetData(typeof(string));
+                if (!_regex.IsMatch(phoneNumber))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private static readonly DependencyProperty ReadonlyProperty =
+            DependencyProperty.Register("Readonly", typeof(bool), typeof(ContactControl).BaseType,
+                new PropertyMetadata(true));
+
+        public bool Readonly
+        {
+            get { return (bool)GetValue(ReadonlyProperty); }
+            set { SetValue(ReadonlyProperty, value); }
+        }
+    }
+}
